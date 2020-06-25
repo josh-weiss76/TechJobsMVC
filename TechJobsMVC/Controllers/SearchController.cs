c668f6b5d5e88d29d7fd70e40166ee099f440bd5
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-25 16:02:18 -0400

    Created an action method to process a search request && Created a Results.cshtml to display the search results

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsMVC.Data;
+using TechJobsMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,9 +16,26 @@ namespace TechJobsMVC.Controllers
         public IActionResult Index()
         {
             ViewBag.columns = ListController.ColumnChoices;
+
             return View();
         }
 
         // TODO #3: Create an action method to process a search request and render the updated search view. 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            List<Job> jobs;
+            if (searchType.ToLower().Equals("all"))
+            {
+                jobs = JobData.FindByValue(searchTerm);
+                ViewBag.results = "Job Search Results";
+            }
+            else
+            {
+                jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.results = "Job Search Results";
+            }
+            ViewBag.jobs = jobs;
+            return View();
+        }
     }
 }
